59ee6012e8de0046730cede0f4d00403967712fe
Aiden Hong <brain1401@gmail.com>
2023-05-29 05:30:54 +0900

    Profile컴포넌트 완성 및 메인 페이지에 삽입

---
+++ src/components/Profile.tsx
@@ -0,0 +1,19 @@
+import Image from "next/image";
+import meImage from "../../public/images/me.png";
+import Link from "next/link";
+
+export default function Profile() {
+  return (
+    <div className="m-auto w-fit text-center">
+      <div className="relative h-60 w-60 overflow-hidden rounded-full object-contain">
+        <Image src={meImage} alt="Aiden Hong" fill priority />
+      </div>
+      <h2 className="text-3xl font-bold">{"Hi, I'm Aiden"}</h2>
+      <h4 className="text-xl font-semibold ">College Student</h4>
+      <p className="mb-3">프론트엔드 개발자를 꿈꾸는 대학생</p>
+      <Link href='/contact' className="appearance-none border border-sky-500 p-1.5 rounded-xl font-bold">
+        Contact Me
+      </Link>
+    </div>
+  );
+}
