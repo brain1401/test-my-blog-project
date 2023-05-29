6a92a4126af1a04fca4af980a9b4e3f3c9a7ba2e
Aiden Hong <brain1401@gmail.com>
2023-05-29 22:01:16 +0900

    category CSS 수정

--- src/components/PostCard.tsx
+++ src/components/PostCard.tsx
@@ -26,7 +26,7 @@ export default function PostCard({ post }: Props) {
           <time className="block text-end mr-4">{post.date.toString()}</time>
           <h2 className="h-7 mt-1 text-center text-xl font-bold">{post.title}</h2>
           <p className="my-2 h-5 text-center text-sm font-semibold">{post.description}</p>
-          <div className="mx-auto my-6 w-28 rounded-xl border border-cyan-200 text-center">
+          <div className="flex items-center justify-center mx-auto my-6 w-28 h-7 text-sm font-semibold rounded-xl border border-cyan-200 text-center">
             {post.category}
           </div>
         </div>
