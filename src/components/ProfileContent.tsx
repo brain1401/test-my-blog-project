142d8ba15d32b9a74a80b790806be7abb9db715b
Aiden Hong <brain1401@gmail.com>
2023-06-02 20:47:20 +0900

    about 페이지 ProfileContent 디자인 변경

--- src/components/ProfileContent.tsx
+++ src/components/ProfileContent.tsx
@@ -1,13 +1,13 @@
 export default function ProfileContent({title, contents} : {title: string; contents:string[]}) {
 
   return (
-    <>
-      <h2 className="m-3 text-2xl font-bold">{title}</h2>
-      <div className="m-3 text-lg">
+    <div className="m-5">
+      <h2 className="text-2xl font-bold">{title}</h2>
+      <div className="text-lg">
         {contents.map((content) => (
           <p key={content}>{content}</p>
         ))}
       </div>
-    </>
+    </div>
   );
 }
