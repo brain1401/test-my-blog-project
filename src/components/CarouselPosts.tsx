f0e693aa42490930ff9be7c01192c0206fee5315
Aiden Hong <brain1401@gmail.com>
2023-06-03 20:24:14 +0900

    메인 페이지에서 전체 포스트 글자 클릭했을때 posts로 이동 구현

--- src/components/CarouselPosts.tsx
+++ src/components/CarouselPosts.tsx
@@ -1,6 +1,7 @@
 import { getPosts } from "@/service/post";
 import PostCard from "./PostCard";
 import MultiCarousel from "./MultiCarousel";
+import Link from "next/link";
 
 export default async function CarouselCase() {
   let posts = await getPosts();
@@ -9,7 +10,12 @@ export default async function CarouselCase() {
   return (
     <section className="m-10">
       <div className="mx-auto my-3 w-11/12 ">
-        <h2 className="m-2 text-xl border-b w-full border-cyan-400 font-semibold">전체 포스트</h2>
+        <Link href="/posts">
+          <h2 className="m-2 w-full border-b border-cyan-400 text-xl font-semibold">
+            전체 포스트
+          </h2>
+        </Link>
+
         <MultiCarousel>
           {posts.map((post) => (
             <PostCard margin="0" key={post.path} post={post} />
